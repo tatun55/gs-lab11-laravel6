0e39533a637e9a014da884f980207b3c1d726e19
tatun55 <tatun55@gmail.com>
2021-07-18 22:32:49 +0900

    add seeder

---
+++ backend/database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->create([
+            'name' => "TDさんの近所の住人",
+            'email' => "neighborhood@kinjo.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
