473742c79fc30b3e3cc51d2957212ffdfa19f401
tatun55 <tatun55@gmail.com>
2021-08-09 17:42:40 +0900

    fix seeder

--- backend/database/seeds/UserSeeder.php
+++ backend/database/seeds/UserSeeder.php
@@ -18,5 +18,13 @@ class UserSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ]);
+        factory(App\User::class)->create([
+            'role' => "admin",
+            'name' => "ジーズ山崎",
+            'email' => "yamazaki@gs.academy",
+            'email_verified_at' => now(),
+            'password' => Hash::make('adminadmin'),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
