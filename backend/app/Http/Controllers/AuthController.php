5ae723a7cba8c1871e02642eff3dfbe17298b418
tatun55 <tatun55@gmail.com>
2021-08-10 14:53:08 +0900

    fix

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -24,7 +24,6 @@ class AuthController extends Controller
      */
     public function login()
     {
-        return "login";
         $credentials = request(['email', 'password']);
 
         if (!$token = auth()->attempt($credentials)) {
